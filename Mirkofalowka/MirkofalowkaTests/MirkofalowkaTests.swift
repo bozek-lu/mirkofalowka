d4edf14b69a72f65fa727c893e319822b81d0434
Lu <bozek.lu@gmail.com>
2016-08-22 23:23:42 +0200

    Init

---
+++ Mirkofalowka/MirkofalowkaTests/MirkofalowkaTests.swift
@@ -0,0 +1,36 @@
+//
+//  MirkofalowkaTests.swift
+//  MirkofalowkaTests
+//
+//  Created by Lu on 22/08/2016.
+//  Copyright © 2016 wczoraj. All rights reserved.
+//
+
+import XCTest
+@testable import Mirkofalowka
+
+class MirkofalowkaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
