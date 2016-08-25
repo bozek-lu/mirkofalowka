d2be1fff17758ee63c529d2ecf630a864ab5dc47
Lu <bozek.lu@gmail.com>
2016-08-25 17:33:54 +0200

    Pods

---
+++ Mirkofalowka/Mirkofalowka/LoginManager.swift
@@ -0,0 +1,13 @@
+//
+//  LoginManager.swift
+//  Mirkofalowka
+//
+//  Created by Lu on 25/08/2016.
+//  Copyright © 2016 wczoraj. All rights reserved.
+//
+
+import Foundation
+
+class LoginManager {
+    static let address = "a.wykop.api"
+}
